3b4f094a869a9fe265a7bb3ff8c5371c3eef13c2
gustavo <gustavocr@outlook.com>
2020-01-20 14:31:58 -0300

    Added rtl pipe.sv file for the pipe project, some modfification at gitignore and dpram_agent.sv file

--- dpram/testbench/dpram_agent.sv
+++ dpram/testbench/dpram_agent.sv
@@ -5,11 +5,15 @@ class DPRAM_Agent extends uvm_agent;
 
   //  Group: Config
   
+  // todo: Transaction Class
+  // Analysis Port
+  uvm_analysis_port #(DPRAM_Transaction) dpram_agent_analysis_port;
 
-  //  Group: Variables
-  
-
-  //  Group: Functions
+  // todo: Agent components
+  // Agent components declaration
+  // DPRAM_Sequencer dpram_sequencer;
+  // DPRAM_Driver    dpram_driver;
+  // DPRAM_Monitor   dpram_monitor;
 
   //  Constructor: new
   function new(string name = "DPRAM_Agent", uvm_component parent);
@@ -18,91 +22,27 @@ class DPRAM_Agent extends uvm_agent;
     
   endfunction: new
 
-  /*---  UVM Build Phases            ---*/
-  /*------------------------------------*/
   //  Function: build_phase
-  extern function void build_phase(uvm_phase phase);
+  function void build_phase(uvm_phase phase);
+    /*  note: Do not call super.build_phase() from any class that is extended from an UVM base class!  */
+    /*  For more information see UVM Cookbook v1800.2 p.503  */
+    // super.build_phase(phase);
+
+    // Instance of the Analysis Port
+    this.dpram_agent_analysis_port = new("dpram_agent_analysis_port", this);
+
+    // dpram_sequencer = DPRAM_Sequencer::type_id::create("dpram_sequencer", this);
+    // dpram_driver = DPRAM_Driver::type_id::create("dpram_driver", this);
+    // dpram_monitor = DPRAM_Monitor::type_id::create("dpram_monitor", this);
+
+  endfunction: build_phase
+
   //  Function: connect_phase
-  extern function void connect_phase(uvm_phase phase);
-  //  Function: end_of_elaboration_phase
-  extern function void end_of_elaboration_phase(uvm_phase phase);
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
 
-  /*---  UVM Run Phases              ---*/
-  /*------------------------------------*/
-  //  Function: start_of_simulation_phase
-  extern function void start_of_simulation_phase(uvm_phase phase);
-  //  Function: reset_phase
-  extern task reset_phase(uvm_phase phase);
-  //  Function: configure_phase
-  extern task configure_phase(uvm_phase phase);
-  //  Function: main_phase
-  extern task main_phase(uvm_phase phase);
-  //  Function: shutdown_phase
-  extern task shutdown_phase(uvm_phase phase);
-
-  /*---  UVM Cleanup Phases          ---*/
-  /*------------------------------------*/
-  //  Function: extract_phase
-  extern function void extract_phase(uvm_phase phase);
-  //  Function: report_phase
-  extern function void report_phase(uvm_phase phase);
+    // todo: connect driver and sequencer port-export
+    // todo: connect monitor and agent analysis port
+  endfunction: connect_phase  
   
 endclass: DPRAM_Agent
-
-
-/*----------------------------------------------------------------------------*/
-/*  UVM Build Phases                                                          */
-/*----------------------------------------------------------------------------*/
-function void DPRAM_Agent::build_phase(uvm_phase phase);
-  /*  note: Do not call super.build_phase() from any class that is extended from an UVM base class!  */
-  /*  For more information see UVM Cookbook v1800.2 p.503  */
-  // super.build_phase(phase);
-endfunction: build_phase
-
-
-function void DPRAM_Agent::connect_phase(uvm_phase phase);
-  super.connect_phase(phase);
-endfunction: connect_phase
-
-
-function void DPRAM_Agent::end_of_elaboration_phase(uvm_phase phase);
-  super.end_of_elaboration_phase(phase);
-endfunction: end_of_elaboration_phase
-
-
-/*----------------------------------------------------------------------------*/
-/*  UVM Run Phases                                                            */
-/*----------------------------------------------------------------------------*/
-function void DPRAM_Agent::start_of_simulation_phase(uvm_phase phase);
-  super.start_of_simulation_phase(phase);
-endfunction: start_of_simulation_phase
-
-
-task DPRAM_Agent::reset_phase(uvm_phase phase);
-endtask: reset_phase
-
-
-task DPRAM_Agent::configure_phase(uvm_phase phase);
-endtask: configure_phase
-
-
-task DPRAM_Agent::main_phase(uvm_phase phase);
-endtask: main_phase
-
-
-task DPRAM_Agent::shutdown_phase(uvm_phase phase);
-endtask: shutdown_phase
-
-
-/*----------------------------------------------------------------------------*/
-/*  UVM Cleanup Phases                                                        */
-/*----------------------------------------------------------------------------*/
-function void DPRAM_Agent::report_phase(uvm_phase phase);
-  super.report_phase(phase);
-endfunction: report_phase
-
-
-function void DPRAM_Agent::extract_phase(uvm_phase phase);
-  super.extract_phase(phase);
-endfunction: extract_phase
-
